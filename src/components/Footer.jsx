8d27b94da315138e5d64d25e8ef26d16bd664d0d
Gerardo Lucero <gerardo.lucero.c@usach.cl>
2022-12-07 02:57:51 -0300

    Texto del footer editado

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import "./Footer.css";
 import {Link} from "react-router-dom"
+import { Box, IconButton, Text } from "@chakra-ui/react";
+import { PhoneIcon, AddIcon, WarningIcon } from '@chakra-ui/icon'
+import { FaPizzaSlice } from "react-icons/fa";
 
 function Footer() {
   const linkStyle = {
@@ -16,13 +19,13 @@ function Footer() {
             <li>Home</li>
           </Link>
           <Link to="/bikes" style={linkStyle}>
-            <li>Bikes</li>
+            <li>Servicio Al Cliente</li>
           </Link>
           <Link to="/about" style={linkStyle}>
-            <li>About Us</li>
+            <li>Legal</li>
           </Link>
           <Link to="/contact" style={linkStyle}>
-            <li>Contact Us</li>
+            <li>Siguenos</li>
           </Link>
         </ul>
       </div>
@@ -43,52 +46,38 @@ function Footer() {
           </a>
         </div>
         <div className="footer_col">
-          <h3>Electric bikes</h3>
+          <h3>Ayuda</h3>
           <a href="https://www.trekbikes.com/us/en_US/bikes/hybrid-bikes/electric-hybrid-bikes/verve/verve-2/p/28299/?colorCode=blue">
-            Verve+ 2
+            Preguntas Frecuentes
           </a>
           <a href="https://www.trekbikes.com/us/en_US/bikes/mountain-bikes/electric-mountain-bikes/e-caliber/e-caliber-9-6/p/34353/?colorCode=greydark_black">
-            E-Caliber 9.6
-          </a>
-          <a href="https://www.trekbikes.com/us/en_US/bikes/road-bikes/performance-road-bikes/domane/domane-hp/domane-hp/p/30931/?colorCode=grey_black">
-            Domane+ HP
+            Contacto
           </a>
         </div>
         <div className="footer_col">
-          <h3>Mountain bikes</h3>
           <a href="https://www.trekbikes.com/us/en_US/bikes/mountain-bikes/cross-country-mountain-bikes/marlin/marlin-5/p/34587/?colorCode=greenvisibility_teal">
-            Marlin 5
+            Término y Condiciones
           </a>
           <a href="https://www.trekbikes.com/us/en_US/bikes/mountain-bikes/downhill-mountain-bikes/session/session-9-x01/p/34625/?colorCode=bluedark_blue">
-            Session 9 X01
-          </a>
-          <a href="https://www.trekbikes.com/us/en_US/bikes/mountain-bikes/trail-mountain-bikes/roscoe/roscoe-8/p/28487/?colorCode=bluedark_grey">
-            Roscoe 8
-          </a>
-          <a href="https://www.trekbikes.com/us/en_US/bikes/mountain-bikes/trail-mountain-bikes/remedy/remedy-9-8/p/33067/?colorCode=greendark_black">
-            Remedy 9.8
+            Política de Privacidad
           </a>
         </div>
         <div className="footer_col">
-          <h3>Buy equipments</h3>
-          <a href="https://www.trekbikes.com/us/en_US/equipment/cycling-components/bike-tires/c/E208/">
-            Tyres
+          <h3>Redes Sociales</h3>
+          <a href="https://www.facebook.com/">
+           Facebook        
           </a>
-          <a href="https://www.trekbikes.com/us/en_US/equipment/cycling-components/bike-wheels/c/E209/">
-            Wheels
+          <a href="https://twitter.com/">
+            Twitter
           </a>
-          <a href="https://www.trekbikes.com/us/en_US/helmets/">Helmets</a>
-          <a href="https://www.trekbikes.com/us/en_US/equipment/cycling-components/bike-brakes-levers/c/E201/">
-            Brakes &amp; Levers
-          </a>
-          <a href="https://www.trekbikes.com/us/en_US/equipment/cycling-components/bike-pedals/c/E204/">
-            Pedals
+          <a href="https://www.instagram.com/">
+            Instagram
           </a>
         </div>
       </div>
       <br />
       <br />
-      <p id="footer_cp_text">&copy;rideontrek.com &nbsp; 2021</p>
+      <p id="footer_cp_text">&copy;pizzahouse.com &nbsp; 2022</p>
     </div>
   );
 }
